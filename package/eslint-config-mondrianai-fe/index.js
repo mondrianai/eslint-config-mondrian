d9903343d24f8932dd13b023880d77d7701b97da
Hyun Oh <hoh@mondrian.ai>
2020-07-29 17:59:54 +0900

    Add prettier configuration

--- package/eslint-config-mondrianai-fe/index.js
+++ package/eslint-config-mondrianai-fe/index.js
@@ -1,17 +1,18 @@
 module.exports = {
   extends: [
-    "eslint-config-airbnb-base",
-    "./rules/errors.js",
-    "./rules/imports.js",
-    "./rules/production.js",
-    "./rules/mondrianai.js",
-    "./rules/typescript.js",
-    "./rules/react.js",
+    'eslint-config-airbnb-base',
+    './rules/errors.js',
+    './rules/imports.js',
+    './rules/production.js',
+    './rules/mondrianai.js',
+    './rules/typescript.js',
+    './rules/react.js',
+    './rules.prettier.js',
   ].map(require.resolve),
   rules: {},
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
